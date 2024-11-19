e49714bdb9672365b27830d56b6bea1107593f44
rsetiawan <rsetiawan@arrivelogistics.com>
2024-11-19 16:40:17 -0600

    Adding .gitignore

--- Tests/UI/example.spec.js
+++ Tests/UI/example.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('basic test', async ({ page }) => {
+test('Navigate to Google and verify title.', async ({ page }) => {
     await page.goto('https://google.com');
     const title = await page.title();
     expect(title).toBe('Google');
