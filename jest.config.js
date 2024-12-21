90ddd3af164a59c0d11f0fa4468d5d75c0ca95b6
Rega <44708903+msclmn@users.noreply.github.com>
2024-12-21 17:36:23 -0600

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
 // Used for the Test Reporter in GitHub Actions pipeline
 
 module.exports = {
-  testResultsProcessor: "jest-junit",
-  reporters: ["default", "jest-junit"],
-  testResultsProcessor: "jest-junit",
-    
-  outputDirectory: "./test-results", 
+  reporters: [
+    "default",
+    ["jest-junit", { 
+      outputDirectory: "./test-results",
+      outputName: "junit.xml",
+    }],
+  ],
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageReporters: ["json", "lcov"],
