bcc729c67f80a1c34864d6e79b227238d9e92087
rsetiawan <rsetiawan@arrivelogistics.com>
2025-01-16 19:44:34 -0600

    fixing

--- Tests/UI/LambdaTestPlayground/TestHomePage.spec.js
+++ Tests/UI/LambdaTestPlayground/TestHomePage.spec.js
@@ -4,13 +4,13 @@ const { runTest } = require('../../SetupTest');
 
 const homePageLink = 'https://ecommerce-playground.lambdatest.io/';
 
-test('Verify primary headers are displayed on the Home page', runTest(async ({ page }) => {
+test('Verify primary headers are displayed on the Home page', runTest(async (page) => {
     const homepage = new HomePage(page);
     await page.goto(`${homePageLink}`);
     expect(await homepage.SharedStep_isHeadersDisplayedCorrectly()).toBe(true, "Headers were not displayed correctly in the Home page.");
 }));
 
-test('Verify carousel behavior in the Home Page', runTest(async ({ page }) => {
+test('Verify carousel behavior in the Home Page', runTest(async (page) => {
     const homepage = new HomePage(page);
     await page.goto(`${homePageLink}`);
     const isCarouselMoving = await homepage.isCarouselMovingCorrectly();
